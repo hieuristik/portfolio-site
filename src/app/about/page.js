a9591257e4e42f69a3351576f4b3ef64387111b8
Andrew Le <andrewle713@gmail.com>
2025-07-28 20:35:06 +0700

    Playing around with next.js and react, more research will be done

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <>
+    <a href="/">Back</a>
+    <h1>About Page</h1>
+    </>
+  );
+}
---
+++ src/app/work/page.js
@@ -0,0 +1,8 @@
+export default function Work() {
+  return (
+    <>
+    <a href="/">Back</a>
+    <h1>Work Page</h1>
+    </>
+  );
+}
