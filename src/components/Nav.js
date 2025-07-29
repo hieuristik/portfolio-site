f6a9ba3dd1296a9749082d058f5545c4d87d0802
Andrew Le <andrewle713@gmail.com>
2025-07-29 14:07:12 +0700

    More learning; refactored site navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,35 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-const Nav = () => {
+let NAV_LINKS = [
+    {
+        label: "About",
+        href: "/about"
+    },
+    {
+        label: "Work",
+        href: "/work"
+    }
+]
+
+export default function Nav() {
+    const pathname = usePathname();
+
     return (
         <nav>
             <ul>
-                <li><Link href="/">BACK</Link></li>
-                <li><Link href="/about">About</Link></li>
-                <li><Link href="/work">Work</Link></li>
+                {pathname !== "/" && (
+                    <li>
+                        <Link href="/">BACK</Link>
+                    </li>
+                )}
+                {NAV_LINKS.map((link) => (
+                    <li key={link.href}>
+                        <Link href={link.href}>{link.label}</Link>
+                    </li>
+                ))}
             </ul>
         </nav>
-    )
+    );
 }
-
-export default Nav;
