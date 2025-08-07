e9a68eec9bbb691820a4b8430f48021a281e9947
Andrew Le <andrewle713@gmail.com>
2025-08-07 11:50:38 +0700

    More styling changes to back button and table of contents

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -20,8 +20,21 @@ export default function Nav() {
                     onClick={() => router.push("/")}
                     className="back-btn z-[9999]"
                     type="button"
+                    aria-label="Go back to home"
                 >
-                    Back
+                    <svg 
+                        width="20" 
+                        height="20" 
+                        viewBox="0 0 24 24" 
+                        fill="none" 
+                        stroke="currentColor" 
+                        strokeWidth="2" 
+                        strokeLinecap="round" 
+                        strokeLinejoin="round"
+                        className="back-icon"
+                    >
+                        <path d="m15 18-6-6 6-6"/>
+                    </svg>
                 </button>
             )}
             <nav className="navbar z-[9999]">
