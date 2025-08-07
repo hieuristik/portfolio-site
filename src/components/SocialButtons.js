09c895091ac4a235e8fe780135780b08796bd040
Andrew Le <andrewle713@gmail.com>
2025-08-07 11:00:43 +0700

    Styling changes to social buttons

--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -1,22 +1,25 @@
 import React from "react";
+import "../app/socialButtonsStyles.css";
+import { PiLinkedinLogoBold, PiGithubLogoBold } from "react-icons/pi";
 
 export default function SocialButtons() {
   return (
-    <div>
+    <div className="social-buttons-container">
       <a
         href="https://www.linkedin.com/in/leltrunghieu1017"
         target="_blank"
         rel="noopener noreferrer"
+        className="social-button linkedin"
       >
-        LinkedIn 
+        <PiLinkedinLogoBold />
       </a>
-      <br />
       <a
         href="https://github.com/hieuristik"
         target="_blank"
         rel="noopener noreferrer"
+        className="social-button github"
       >
-        GitHub
+        <PiGithubLogoBold />
       </a>
     </div>
   );
