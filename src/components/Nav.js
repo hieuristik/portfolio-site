5d211226ccd19e93f5662b64bad82bab1c486869
Andrew Le <andrewle713@gmail.com>
2025-08-07 11:41:23 +0700

    Nav styling changed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,8 +5,8 @@ import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
 
 let NAV_LINKS = [
-    { label: "About", href: "/about" },
-    { label: "Work", href: "/work" },
+    { label: "ABOUT", href: "/about" },
+    { label: "WORK", href: "/work" },
 ];
 
 export default function Nav() {
