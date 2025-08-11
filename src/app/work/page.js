ccb33a0e98ee256dece14a3ab32b39ec4809c026
Andrew Le <andrewle713@gmail.com>
2025-08-11 12:42:19 +0700

    Initializing Work page

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -1,7 +1,23 @@
+import Footer from "@/components/Footer";
+
 export default function Work() {
   return (
-    <div className="pt-24 px-6 flex justify-between items-start">
-      <h1>Work Page</h1>
+    <div className="pt-24 px-6 min-h-screen">
+      <div className="max-w-7xl mx-auto">
+        <div className="main-content space-y-4 max-w-4xl">
+          <h1 id="dev-projects">DEV PROJECTS</h1>
+          <p>Lorem ipsum dolor sit amet. Qui accusantium eligendi ea ipsa delectus qui illum quibusdam ut natus voluptatem aut culpa quia sed nobis voluptas. Ad accusantium nesciunt qui quia facilis 33 iusto voluptatem. </p><p>Aut voluptas quod 33 obcaecati perferendis est nobis exercitationem sit alias debitis ut mollitia iure non laborum voluptas. Aut suscipit omnis et deserunt nesciunt ea possimus accusantium est quia aliquam est nemo ipsam sit blanditiis omnis quo nihil quia. In corrupti quia sit quia optio aut unde doloremque? </p><p>Vel quia autem ut vitae error et obcaecati consectetur sed soluta necessitatibus ut debitis deserunt non quia omnis et modi accusantium. Ut maxime nihil qui placeat laudantium et repellendus incidunt quo earum vero eum reprehenderit voluptatibus id amet fuga. Est ipsa saepe quo ipsum quia ut labore sint sed saepe distinctio in voluptatem consequatur. Et impedit voluptas At vitae pariatur id ducimus porro eos error quia? </p>
+          <br></br>
+          <h1 id="photography">PHOTOGRAPHY</h1>
+          <p>Lorem ipsum dolor sit amet. Qui accusantium eligendi ea ipsa delectus qui illum quibusdam ut natus voluptatem aut culpa quia sed nobis voluptas. Ad accusantium nesciunt qui quia facilis 33 iusto voluptatem. </p><p>Aut voluptas quod 33 obcaecati perferendis est nobis exercitationem sit alias debitis ut mollitia iure non laborum voluptas. Aut suscipit omnis et deserunt nesciunt ea possimus accusantium est quia aliquam est nemo ipsam sit blanditiis omnis quo nihil quia. In corrupti quia sit quia optio aut unde doloremque? </p><p>Vel quia autem ut vitae error et obcaecati consectetur sed soluta necessitatibus ut debitis deserunt non quia omnis et modi accusantium. Ut maxime nihil qui placeat laudantium et repellendus incidunt quo earum vero eum reprehenderit voluptatibus id amet fuga. Est ipsa saepe quo ipsum quia ut labore sint sed saepe distinctio in voluptatem consequatur. Et impedit voluptas At vitae pariatur id ducimus porro eos error quia? </p>
+          <br></br>
+          <h1 id="video">VIDEO</h1>
+          <p>Lorem ipsum dolor sit amet. Qui accusantium eligendi ea ipsa delectus qui illum quibusdam ut natus voluptatem aut culpa quia sed nobis voluptas. Ad accusantium nesciunt qui quia facilis 33 iusto voluptatem. </p><p>Aut voluptas quod 33 obcaecati perferendis est nobis exercitationem sit alias debitis ut mollitia iure non laborum voluptas. Aut suscipit omnis et deserunt nesciunt ea possimus accusantium est quia aliquam est nemo ipsam sit blanditiis omnis quo nihil quia. In corrupti quia sit quia optio aut unde doloremque? </p><p>Vel quia autem ut vitae error et obcaecati consectetur sed soluta necessitatibus ut debitis deserunt non quia omnis et modi accusantium. Ut maxime nihil qui placeat laudantium et repellendus incidunt quo earum vero eum reprehenderit voluptatibus id amet fuga. Est ipsa saepe quo ipsum quia ut labore sint sed saepe distinctio in voluptatem consequatur. Et impedit voluptas At vitae pariatur id ducimus porro eos error quia? </p>
+          <br></br>
+        </div>
+      </div>
+      <br></br>
+      <Footer />
     </div>
   );
 }
