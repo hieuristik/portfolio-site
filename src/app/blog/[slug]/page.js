b0f087ae023fe6d2488dceaa93031bdc2dfdb4d1
Andrew Le <andrewle713@gmail.com>
2025-08-15 11:15:08 +0700

    Blog directory and page setup

---
+++ src/app/blog/[slug]/page.js
@@ -0,0 +1,14 @@
+export default function BlogPage({ params }) {
+  const blog = blogData.find((b) => b.slug === params.slug);
+
+  if (!blog) return <div className="p-10">Blog not found</div>;
+
+  return (
+    <div className="pt-24 px-6 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4">{blog.title}</h1>
+      <img src={blog.image} alt={blog.title} className="rounded-xl mb-4" />
+      <p className="text-lg">{blog.description}</p>
+      {/* Add more content as needed */}
+    </div>
+  );
+}
